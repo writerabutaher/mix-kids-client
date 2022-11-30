918dab8e1a260edcdc9fa953060ef41a934014c4
Abu Taher <writerabutaher@gmail.com>
2022-11-30 21:30:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex items-center justify-center h-screen animate-bounce">
+        <h1 className="text-5xl">Hello World</h1>
+      </div>
+    </>
   );
 }
 
